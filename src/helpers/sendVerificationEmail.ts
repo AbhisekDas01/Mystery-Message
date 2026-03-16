1ce3a56d1fc16ce7c53bf9ad29856c59e62d96e3
AbhisekDas01 <dasabhisek668@gmail.com>
2026-03-16 23:51:31 +0530

    created sign-up api route and signup verification email with resend

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,24 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../email/verificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string,
+): Promise<ApiResponse> {
+  try {
+        await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: email,
+            subject: "Verify Your Account",
+            react: VerificationEmail({username , otp: verifyCode}),
+        });
+
+        return { success: true, message: "Verification Email Sent" };
+  } catch (error) {
+        console.error("Error sending verification Email: ", error);
+        return { success: false, message: "Failed to Send verification Email" };
+  }
+}
